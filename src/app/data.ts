b1a06cd00f21eb5739d4e6a667734e3dfaf63136
Cami Gonzalez <castegong@gmail.com>
2021-07-18 23:51:42 -0400

    Added consultation message and main button

--- src/app/data.ts
+++ src/app/data.ts
@@ -8,5 +8,9 @@ export const TEXTS = {
   MAIN_DESCRIPTION: 'Somos un centro médico especializado, bajo la dirección de la Dra. Lillian Arias, Médico Cirujano General, Médico Tibetano y Acupunturista. Ozonoterapeuta, Médico Biorregulador y Médico Ortomolecular. Con amplio conocimiento en la aplicación de la Medicina Integrativa.',
   TITLE_SECTION: {
     INTEGRATIVE: 'Medicina Integrativa'
+  },
+  CONSULTATION_DESCRIPTION: 'Para realizar un tratamiento de Medicina Integrativa, es necesario realizar una Consulta Médica previa que incluye un Escáner Biocuántico.',
+  BUTTONS: {
+    CONTACT: 'Contactar'
   }
 }
